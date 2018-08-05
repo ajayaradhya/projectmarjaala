b438d6715e23336840b572349afa68664cf4dfc8
ajayaradhya <be.el.ajay@gmail.com>
2018-08-05 20:51:26 +0530

    Moving blocks to left

--- ProjectMarjaala/Assets/Scripts/LeftCollider.cs
+++ ProjectMarjaala/Assets/Scripts/LeftCollider.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class LeftCollider : MonoBehaviour {
 
+    [SerializeField] GameObject sceneController;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("collision took place. deleting the collided object.");
-        Destroy(collision.collider.gameObject);
+        Destroy(collision.collider.gameObject, 1);
+        //sceneController.GetComponent<SceneController>().CreateNewBlock();
     }
 }
---
+++ ProjectMarjaala/Assets/Scripts/SceneController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    [SerializeField] GameObject blocksPrefab;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Instantiate(blocksPrefab, parentForBlocks.transform);
+	}
+
+    public void CreateNewBlock()
+    {
+        Debug.Log("Creating..");
+        var newBlock = Instantiate(blocksPrefab);
+        Destroy(newBlock, 5);
+    }
+}
