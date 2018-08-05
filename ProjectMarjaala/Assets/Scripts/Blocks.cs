c32096b1794d5e36b4e01642c116d6ea054e6380
ajayaradhya <be.el.ajay@gmail.com>
2018-08-05 19:59:12 +0530

    Adding colliders

---
+++ ProjectMarjaala/Assets/Scripts/Blocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var pos = new Vector2(gameObject.transform.position.x - 0.5f, gameObject.transform.position.y);
+        gameObject.transform.position = pos;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("blocks collided");
+    }
+}
---
+++ ProjectMarjaala/Assets/Scripts/LeftCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftCollider : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision took place. deleting the collided object.");
+        Destroy(collision.collider.gameObject);
+    }
+}
